d8724ab24a7cd921afe184363ef501f70d72f2f6
Henry <193541T@mymail.nyp.edu.sg>
2021-02-14 11:31:55 +0800

    BIG

---
+++ IdolFever/Assets/Scripts/ConnectionStatus.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace IdolFever {
+    internal sealed class ConnectionStatus: MonoBehaviour {
+        #region Fields
+
+        private readonly string textFront = "Connection Status: ";
+        [SerializeField] private Text textComponent;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Update() {
+            textComponent.text = textFront + PhotonNetwork.NetworkClientState;
+        }
+
+        #endregion
+    }
+}
---
+++ IdolFever/Assets/Scripts/EventSystemSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace IdolFever {
+    internal sealed class EventSystemSpawner: MonoBehaviour {
+		#region Fields
+		#endregion
+
+		#region Properties
+		#endregion
+
+		#region Unity User Callback Event Funcs
+
+		private void OnEnable() {
+			EventSystem sceneEventSystem = FindObjectOfType<EventSystem>();
+			if(sceneEventSystem == null) {
+				GameObject eventSystem = new GameObject("EventSystem");
+				eventSystem.AddComponent<EventSystem>();
+				eventSystem.AddComponent<StandaloneInputModule>();
+			}
+		}
+
+		#endregion
+	}
+}
