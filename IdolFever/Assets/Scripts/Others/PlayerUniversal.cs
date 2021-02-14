7adeb48886950176eaaedfb30247f4ad4fdb3946
Henry <193541T@mymail.nyp.edu.sg>
2021-02-14 17:09:38 +0800

    Very HUGE

---
+++ IdolFever/Assets/Scripts/Others/PlayerUniversal.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace IdolFever {
+    internal static class PlayerUniversal {
+        #region Fields
+
+        private static Color[] colors;
+
+        #endregion
+
+        #region Properties
+
+        public static Color[] Colors {
+            get {
+                return colors;
+            }
+            set {
+                colors = value;
+            }
+        }
+
+        #endregion
+
+        static PlayerUniversal() {
+            colors = System.Array.Empty<Color>();
+        }
+
+        public static void InitColors() {
+            float commonVal = Random.Range(0.0f, 1.0f);
+
+            colors = new Color[]{
+                new Color(0.0f, 0.0f, 0.0f, 1.0f),
+                new Color(1.0f, 1.0f, 1.0f, 1.0f),
+                new Color(commonVal, commonVal, commonVal, 1.0f),
+                new Color(Random.Range(0.2f, 0.8f), 0.0f, 0.0f, 1.0f),
+                new Color(0.0f, Random.Range(0.2f, 0.8f), 0.0f, 1.0f),
+                new Color(0.0f, 0.0f, Random.Range(0.2f, 0.8f), 1.0f),
+                new Color(Random.Range(0.2f, 0.8f), 0.0f, Random.Range(0.2f, 0.8f), 1.0f),
+                new Color(Random.Range(0.2f, 0.8f), Random.Range(0.2f, 0.8f), 0.0f, 1.0f),
+                new Color(0.0f, Random.Range(0.2f, 0.8f), Random.Range(0.2f, 0.8f), 1.0f),
+                new Color(0.82f, 0.41f, 0.11f),
+            };
+            ShuffleElements.Shuffle(colors);
+        }
+    }
+}
