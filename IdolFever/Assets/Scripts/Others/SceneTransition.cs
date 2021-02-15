b1212006ed45054255c8e9f9d1f5fe1f0b007359
Henry <193541T@mymail.nyp.edu.sg>
2021-02-15 10:58:45 +0800

    BlackImg

---
+++ IdolFever/Assets/Scripts/Others/SceneTransition.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace IdolFever {
+    internal sealed class SceneTransition: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private Animator animator;
+        [SerializeField] private string sceneName;
+        [SerializeField] private float transitionTime;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+	    private void Update() {
+            if(Input.GetMouseButtonDown(0)) {
+                _ = StartCoroutine(SceneTransitionCoroutine(sceneName));
+            }
+	    }
+
+        private System.Collections.IEnumerator SceneTransitionCoroutine(string sceneName) {
+            animator.SetTrigger("Start");
+
+            yield return new WaitForSeconds(transitionTime);
+
+            SceneManager.LoadScene(sceneName);
+        }
+
+        #endregion
+
+        public SceneTransition() {
+            animator = null;
+            sceneName = string.Empty;
+            transitionTime = 0.0f;
+        }
+    }
+}
