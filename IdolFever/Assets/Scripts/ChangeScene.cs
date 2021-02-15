cdec8c871d09007486eb2f8e29761e359484dc07
Elaine0202 <54051099+Elaine0202@users.noreply.github.com>
2021-02-15 22:59:07 +0800

    button added
    button added

---
+++ IdolFever/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string changeTo;
+
+    public void ChangetoScene()
+    {
+        if (changeTo.Length <= 0)
+            Debug.Log("There is no scene to change to.");
+        else
+        {
+            SceneManager.LoadScene(changeTo);
+        }
+    }
+}
