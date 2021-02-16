be5a2c422cb610fd365f41d9b7505cb49722bc18
Elaine0202 <54051099+Elaine0202@users.noreply.github.com>
2021-02-16 10:30:51 +0800

    scoremeter
    scoremeter added

---
+++ IdolFever/Assets/Scripts/ScoreMeterBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreMeterBar : MonoBehaviour
+{
+    public Slider scoremeter;
+    public ScoreMeter playerscoreMeter;
+
+    private void Start()
+    {
+        playerscoreMeter = GameObject.FindGameObjectWithTag("ScoreMeters").GetComponent<ScoreMeter>();
+        scoremeter = GetComponent<Slider>();
+        scoremeter.maxValue = playerscoreMeter.maxScore;
+        scoremeter.value = playerscoreMeter.currScore;
+    }
+
+    public void SetScore(int score)
+    {
+        scoremeter.value = score;
+    }
+}
