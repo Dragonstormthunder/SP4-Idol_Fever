9e3cc8f9f1e252ab6872a72a39babf21b0a42a4b
HenIsTheMan <54048527+HenIsTheMan@users.noreply.github.com>
2021-02-18 15:50:11 +0800

    Revert "test"
    This reverts commit 016966af9feea90abfe75efcf27aa5f6244f12c1.

---
+++ IdolFever/Assets/Scripts/GuanYu/MusicSrcVolControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class MusicSrcVolControl: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private AudioSource audioSrc;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+	    private void FixedUpdate() {
+	        audioSrc.volume = Options.MusicVol;
+	    }
+
+        #endregion
+
+        public MusicSrcVolControl() {
+            audioSrc = null;
+        }
+    }
+}
