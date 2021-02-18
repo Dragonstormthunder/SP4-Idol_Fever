57e64cfa4a90f182f1975a47fcc5dfcd1874bd9b
HenIsTheMan <54048527+HenIsTheMan@users.noreply.github.com>
2021-02-18 11:56:57 +0800

    save

---
+++ IdolFever/Assets/Scripts/GuanYu/SelectionControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class SelectionControl: MonoBehaviour {
+
+        [System.Serializable]
+        public struct PosXY { //??
+            public float x;
+            public float y;
+        }
+
+        #region Fields
+
+        [SerializeField] private GraphicsQualityOptions.GraphicsQualityOption currOption;
+        [SerializeField] private PosXY[] pos;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+	    private void Update() {
+	    
+	    }
+
+        #endregion
+
+        public SelectionControl() {
+            currOption = GraphicsQualityOptions.GraphicsQualityOption.NotAnOption;
+            pos = System.Array.Empty<PosXY>();
+        }
+    }
+}
