016966af9feea90abfe75efcf27aa5f6244f12c1
HenIsTheMan <54048527+HenIsTheMan@users.noreply.github.com>
2021-02-18 15:49:56 +0800

    test

---
+++ IdolFever/Assets/Scripts/GuanYu/MusicVolControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class MusicVolControl: MonoBehaviour {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            AudioSource[] audioSrcs = FindObjectsOfType<AudioSource>();
+            foreach(AudioSource audioSrc in audioSrcs) {
+                audioSrc.volume = Options.MusicVol;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ IdolFever/Assets/Scripts/GuanYu/SoundVolControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class SoundVolControl: MonoBehaviour {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+/*            AudioSource[] audioSrcs = FindObjectsOfType<AudioSource>();
+            foreach(AudioSource audioSrc in audioSrcs) {
+                Debug.Log("here", this);
+                audioSrc.volume = Options.SoundVol;
+            }*/
+        }
+
+        #endregion
+    }
+}
