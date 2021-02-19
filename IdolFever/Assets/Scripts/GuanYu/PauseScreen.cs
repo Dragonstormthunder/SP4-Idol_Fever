96d605b6eab59c905e472917558bae76ca95b7fb
Henry <193541T@mymail.nyp.edu.sg>
2021-02-19 11:19:35 +0800

    PauseScreen.cs

---
+++ IdolFever/Assets/Scripts/GuanYu/PauseScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class PauseScreen: MonoBehaviour {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+        #endregion
+
+        public void ActivateSelf() {
+            gameObject.SetActive(true);
+        }
+
+        public void DeactivateSelf() {
+            gameObject.SetActive(false);
+        }
+    }
+}
