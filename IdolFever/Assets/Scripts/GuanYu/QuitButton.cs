7a6c67cbba9e0c28fad8f96eb0e50b1c32477904
Henry <193541T@mymail.nyp.edu.sg>
2021-02-19 22:33:31 +0800

    QuitButton.cs

---
+++ IdolFever/Assets/Scripts/GuanYu/QuitButton.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class QuitButton: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private AsyncSceneTransitionOut asyncSceneTransitionOut;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+        #endregion
+
+        public QuitButton() {
+            asyncSceneTransitionOut = null;
+        }
+
+        public void OnClick() {
+            _ = StartCoroutine(nameof(DisconnectAndChangeScene));
+        }
+
+        private System.Collections.IEnumerator DisconnectAndChangeScene() {
+            PhotonNetwork.Disconnect();
+
+            while(PhotonNetwork.IsConnected) {
+                yield return null;
+            }
+
+            asyncSceneTransitionOut.ChangeScene();
+        }
+    }
+}
