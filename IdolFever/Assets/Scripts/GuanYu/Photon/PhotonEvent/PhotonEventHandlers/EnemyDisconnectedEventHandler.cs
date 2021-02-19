ffd2b627995d3d0b3e9a8299dfde60b233a221e0
Henry <193541T@mymail.nyp.edu.sg>
2021-02-19 23:25:33 +0800

    go go

--- IdolFever/Assets/Scripts/GuanYu/Photon/PhotonEvent/PhotonEventHandlers/EnemyDisconnectedEventHandler.cs
+++ IdolFever/Assets/Scripts/GuanYu/Photon/PhotonEvent/PhotonEventHandlers/EnemyDisconnectedEventHandler.cs
@@ -9,6 +9,10 @@ namespace IdolFever {
 
         [SerializeField] private GameObject dcPrefab;
         [SerializeField] private GameObject parent;
+        [SerializeField] private string myName;
+        [SerializeField] private int siblingIndex;
+        [SerializeField] private float x;
+        [SerializeField] private float y;
 
         #endregion
 
@@ -19,11 +23,26 @@ namespace IdolFever {
 
         private EnemyDisconnectedEventHandler() {
             dcPrefab = null;
+            parent = null;
+            myName = string.Empty;
+            siblingIndex = 0;
+            x = 0.0f;
+            y = 0.0f;
         }
 
         #endregion
 
         #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            GameObject dcGO = Instantiate(dcPrefab, new Vector3(x, y, 0.0f), Quaternion.identity);
+            dcGO.name = myName;
+
+            if(parent != null) {
+                dcGO.transform.SetParent(parent.transform, false);
+                dcGO.transform.SetSiblingIndex(siblingIndex);
+            }
+        }
 
         private void OnEnable() {
             PhotonNetwork.NetworkingClient.EventReceived += OnEvent;
@@ -37,10 +56,12 @@ namespace IdolFever {
 
         public void OnEvent(EventData photonEvent) {
             if(photonEvent.Code == (byte)EventCodes.EventCode.EnemyDisconnectedEvent) {
-                GameObject dcGO = Instantiate(dcPrefab);
+                GameObject dcGO = Instantiate(dcPrefab, new Vector3(x, y, 0.0f), Quaternion.identity);
+                dcGO.name = myName;
 
                 if(parent != null) {
-                    dcGO.transform.SetParent(parent.transform);
+                    dcGO.transform.SetParent(parent.transform, false);
+                    dcGO.transform.SetSiblingIndex(siblingIndex);
                 }
             }
         }
