8ade23f825e397bbb56bc25d9876a0605f6741d7
Elaine0202 <54051099+Elaine0202@users.noreply.github.com>
2021-02-19 12:10:13 +0800

    countdown in game Scene

---
+++ IdolFever/Assets/Scripts/CountDownController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CountDownController : MonoBehaviour
+{
+    public int countDownTime;
+    public TMP_Text countDownDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CountDownToStart());
+    }
+
+    IEnumerator CountDownToStart()
+    {
+        while (countDownTime > 0)
+        {
+            countDownDisplay.text = countDownTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+            --countDownTime;
+        }
+        countDownDisplay.text = "Go !";
+
+        yield return new WaitForSeconds(1f);
+
+        countDownDisplay.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
