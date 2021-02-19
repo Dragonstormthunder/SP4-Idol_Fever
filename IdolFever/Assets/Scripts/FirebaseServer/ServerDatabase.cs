2bd2cde0512eee78b1d7758cf7d0325be6ec541b
Dragonstormthunder/Ezra <54053973+Dragonstormthunder@users.noreply.github.com>
2021-02-19 14:34:53 +0800

    firebase highscore equipments

--- IdolFever/Assets/Scripts/FirebaseServer/ServerDatabase.cs
+++ IdolFever/Assets/Scripts/FirebaseServer/ServerDatabase.cs
@@ -18,6 +18,7 @@ namespace IdolFever.Server
 
         // a dozen of keys
         public const string DATABASE_USERS = "users";
+        public const string DATABASE_USERNAME = "USERNAME";
         public const string DATABASE_GEM = "GEM";
         public const string DATABASE_ACHIEVEMENT = "ACHIEVEMENT";
         public const string DATABASE_ACHIEVEMENT_CLAIMED = "CLAIMED";
@@ -25,12 +26,12 @@ namespace IdolFever.Server
         public const string DATABASE_CHARACTER_NUMBER = "NUMBER";
         public const string DATABASE_ENERGY = "ENERGY";
         public const string DATABASE_LAST_LOGIN = "LAST_LOGIN";
-        
-        public const string DATABASE_SONG_HIGHSCORE = "SONG_HIGHSCORE";
+
+        //public const string DATABASE_SONG_HIGHSCORE = "SONG_HIGHSCORE";
         #region SONG_DATABASE_KEYS
-        public const string DBSH_MOUNTAIN_KING = "MOUNTAIN_KING";
-        public const string DBSH_ORIGINAL_SONG = "ORIGINAL_SONG";
-        public const string DBSH_WELLERMAN = "WELLERMAN";
+        public const string DBSH_MOUNTAIN_KING = "MOUNTAIN_KING_HIGHSCORE";
+        public const string DBSH_ORIGINAL_SONG = "ORIGINAL_SONG_HIGHSCORE";
+        public const string DBSH_WELLERMAN = "WELLERMAN_SONG_HIGHSCORE";
         #endregion
 
         #endregion
@@ -64,6 +65,14 @@ namespace IdolFever.Server
             StartCoroutine(GrabHighestScoreOfASong(DBSH_MOUNTAIN_KING, (scorer) =>
             {
                 Debug.Log("Highest score: " + scorer);
+            }));
+
+            StartCoroutine(GrabAllScoresOfASong(DBSH_MOUNTAIN_KING, (scores) =>
+            {
+                foreach (KeyValuePair<string, int> keyValuePair in scores)
+                {
+                    Debug.Log("Scoring: " + keyValuePair);
+                }
             }));
 
         }
@@ -166,6 +175,21 @@ namespace IdolFever.Server
                 }
             }
 
+        }
+
+        public IEnumerator UpdateUsername(string username)
+        {
+            // update the value of the username
+            // will create here if it doesn't exist
+            var DBTask = DBreference.Child(DATABASE_USERS).Child(User.UserId).Child(DATABASE_USERNAME).SetValueAsync(username);
+
+            yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+            // error
+            if (DBTask.Exception != null)
+            {
+                Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
+            }
         }
 
         public IEnumerator UpdateAchievements(string achievementName, bool hasClaimed)
@@ -384,7 +408,7 @@ namespace IdolFever.Server
         {
             // update the high score
             // will create here if it doesn't exist
-            var DBTask = DBreference.Child(DATABASE_USERS).Child(User.UserId).Child(DATABASE_SONG_HIGHSCORE).Child(songName).SetValueAsync(highscore);
+            var DBTask = DBreference.Child(DATABASE_USERS).Child(User.UserId).Child(songName).SetValueAsync(highscore);
 
             yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
 
@@ -394,9 +418,9 @@ namespace IdolFever.Server
             }
         }
 
-        public IEnumerator GrabHighestScoreOfASong(string songName, System.Action<int> callbackOnFinish)
+        public IEnumerator GrabHighestScoreOfASong(string songName, System.Action<KeyValuePair<string, int>> callbackOnFinish)
         {
-            // grab the highest score of a song
+            // grab all the scores of a song
             var DBTask = DBreference.Child(DATABASE_USERS).OrderByChild(songName).GetValueAsync();
 
             yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
@@ -404,17 +428,67 @@ namespace IdolFever.Server
             if (DBTask.Exception != null)
             {
                 Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
-                callbackOnFinish(-1);
+                callbackOnFinish(new KeyValuePair<string, int>("", -1));
             }
             else
             {
                 DataSnapshot snapshot = DBTask.Result;
 
-                // loop throuhg every users UID
-                //foreach (DataSnapshot childSnapshot in snapshot.Children.Reverse<DataSnapshot>())
-                //{
-                //
-                //}
+                Debug.Log("Highest score of a song");
+
+                // highest score of a song will be the last one in the snapshot
+                // the scores are sorted via ascending order when retrieving
+                DataSnapshot topScorer = snapshot.Children.Last();
+
+                Debug.Log("Username: " + topScorer.Child(DATABASE_USERNAME).Value.ToString() + "; Score: " + topScorer.Child(songName).Value.ToString());
+
+                callbackOnFinish(new KeyValuePair<string, int>(topScorer.Child(DATABASE_USERNAME).Value.ToString(), int.Parse(topScorer.Child(songName).Value.ToString())));
+
+            }
+
+        }
+
+        public IEnumerator GrabAllScoresOfASong(string songName, System.Action<List<KeyValuePair<string, int>>> callbackOnFinish)
+        {
+
+            // prepare the list
+            List<KeyValuePair<string, int>> songScores = new List<KeyValuePair<string, int>>();
+
+            // grab all the scores of a song
+            var DBTask = DBreference.Child(DATABASE_USERS).OrderByChild(songName).GetValueAsync();
+
+            yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+            if (DBTask.Exception != null)
+            {
+                Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
+                callbackOnFinish(null); // nothing to send anyway
+            }
+            else
+            {
+                DataSnapshot snapshot = DBTask.Result;
+
+                Debug.Log("Highest score of a song");
+
+                // the scores are sorted via ascending order when retrieving
+
+
+                // loop through every users UID
+                foreach (DataSnapshot childSnapshot in snapshot.Children.Reverse())
+                {
+                    // check if the song name exists first
+                    // because if they haven't played the song the song high score may not exist
+                    if (childSnapshot.HasChild(songName))
+                    {
+                        //Debug.Log("Username: " + childSnapshot.Child(DATABASE_USERNAME).Value.ToString() + "; Score: " + childSnapshot.Child(songName).Value.ToString());
+
+                        songScores.Add(new KeyValuePair<string, int>(childSnapshot.Value.ToString(), int.Parse(snapshot.Child(songName).Value.ToString())));
+
+                    }
+
+
+
+                }
 
             }
 
