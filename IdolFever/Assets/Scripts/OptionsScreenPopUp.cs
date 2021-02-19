cc6d747d7c66ab943ecb5a668dab08f0a7572113
Dragonstormthunder/Ezra <54053973+Dragonstormthunder@users.noreply.github.com>
2021-02-19 11:56:07 +0800

    options screen log out/ delete account finished

---
+++ IdolFever/Assets/Scripts/OptionsScreenPopUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OptionsScreenPopUp : MonoBehaviour, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private bool mouseInside;
+
+    // disable the pop up when clicked out
+    public void OnDeselect(BaseEventData eventData)
+    {
+        
+        // check whether the mouse pointer is inside of the panel
+        if (!mouseInside)
+            gameObject.SetActive(false);
+
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouseInside = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseInside = false;
+    }
+}
