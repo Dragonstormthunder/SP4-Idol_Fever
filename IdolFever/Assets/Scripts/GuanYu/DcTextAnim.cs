51d3a1da691b3ea8d85187e56b5dd6f443e74bf8
Henry <193541T@mymail.nyp.edu.sg>
2021-02-20 10:25:33 +0800

    DcTextAnim.cs (untested)

---
+++ IdolFever/Assets/Scripts/GuanYu/DcTextAnim.cs
@@ -0,0 +1,62 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class DcTextAnim: MonoBehaviour {
+        #region Fields
+
+        private float elapsedTime;
+        [SerializeField] private TextMeshProUGUI tmpComponent;
+        [HideInInspector] public float spd;
+        [HideInInspector] public float startY;
+        [HideInInspector] public float endY;
+        [HideInInspector] public float startSize;
+        [HideInInspector] public float endSize;
+        [HideInInspector] public float startAlpha;
+        [HideInInspector] public float endAlpha;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Update() {
+            elapsedTime += Time.deltaTime;
+
+            float lerpFactor = EaseInOutCirc(Mathf.Cos(elapsedTime * spd) * 0.5f + 0.5f);
+
+            tmpComponent.alpha = (1.0f - lerpFactor) * startAlpha + lerpFactor * endAlpha;
+            if(Mathf.Approximately(tmpComponent.alpha, 0.0f)) {
+                Destroy(this);
+                return;
+            }
+
+            RectTransform rectTransform = (RectTransform)transform;
+            rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, (1.0f - lerpFactor) * startY + lerpFactor * endY);
+
+            tmpComponent.fontSize = (1.0f - lerpFactor) * startSize + lerpFactor * endSize;
+        }
+
+        #endregion
+
+        public DcTextAnim() {
+            elapsedTime = 0.0f;
+            tmpComponent = null;
+            spd = 0.0f;
+            startY = 0.0f;
+            endY = 0.0f;
+            startSize = 0.0f;
+            endSize = 0.0f;
+            startAlpha = 0.0f;
+            endAlpha = 0.0f;
+        }
+
+        private float EaseInOutCirc(float x) {
+            return x < 0.5f
+                ? (1.0f - Mathf.Sqrt(1.0f - Mathf.Pow(2.0f * x, 2.0f))) * 0.5f
+                : (Mathf.Sqrt(1.0f - Mathf.Pow(-2.0f * x + 2.0f, 2.0f)) + 1.0f) * 0.5f;
+        }
+    }
+}
--- IdolFever/Assets/Scripts/GuanYu/Photon/PhotonEvent/PhotonEventHandlers/EnemyDisconnectedEventHandler.cs
+++ IdolFever/Assets/Scripts/GuanYu/Photon/PhotonEvent/PhotonEventHandlers/EnemyDisconnectedEventHandler.cs
@@ -11,8 +11,15 @@ namespace IdolFever {
         [SerializeField] private GameObject parent;
         [SerializeField] private string myName;
         [SerializeField] private int siblingIndex;
+
+        [SerializeField] private float spd;
         [SerializeField] private float x;
-        [SerializeField] private float y;
+        [SerializeField] private float startY;
+        [SerializeField] private float endY;
+        [SerializeField] private float startSize;
+        [SerializeField] private float endSize;
+        [SerializeField] private float startAlpha;
+        [SerializeField] private float endAlpha;
 
         #endregion
 
@@ -36,8 +43,15 @@ namespace IdolFever {
             parent = null;
             myName = string.Empty;
             siblingIndex = 0;
+
+            spd = 0.0f;
             x = 0.0f;
-            y = 0.0f;
+            startY = 0.0f;
+            endY = 0.0f;
+            startSize = 0.0f;
+            endSize = 0.0f;
+            startAlpha = 0.0f;
+            endAlpha = 0.0f;
         }
 
         #endregion
@@ -59,8 +73,17 @@ namespace IdolFever {
                 IsLocalPlayer = PhotonNetwork.LocalPlayer.ActorNumber == (int)photonEvent.CustomData;
 
                 if(!IsLocalPlayer) {
-                    GameObject dcGO = Instantiate(dcPrefab, new Vector3(x, y, 0.0f), Quaternion.identity);
+                    GameObject dcGO = Instantiate(dcPrefab, new Vector3(x, startY, 0.0f), Quaternion.identity);
                     dcGO.name = myName;
+
+                    DcTextAnim script = dcGO.GetComponent<DcTextAnim>();
+                    script.spd = spd;
+                    script.startY = startY;
+                    script.endY = endY;
+                    script.startSize = startSize;
+                    script.endSize = endSize;
+                    script.startAlpha = startAlpha;
+                    script.endAlpha = endAlpha;
 
                     if(parent != null) {
                         dcGO.transform.SetParent(parent.transform, false);
