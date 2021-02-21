bd64ceceebc360ca6607c71213771768562753ef
Henry <193541T@mymail.nyp.edu.sg>
2021-02-21 22:54:07 +0800

    FileWriter.cs

---
+++ IdolFever/Assets/Scripts/GuanYu/FileIO/FileWriter.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class FileWriter: MonoBehaviour {
+        #region Fields
+
+        private StreamWriter streamWriter;
+        [SerializeField] private string filePath;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            streamWriter = new StreamWriter(filePath, true);
+            WriteTextToFile("Test");
+        }
+
+        private void OnDisable() {
+            streamWriter.Close();
+        }
+
+        #endregion
+
+        public FileWriter() {
+            streamWriter = null;
+            filePath = string.Empty;
+        }
+
+        public void WriteTextToFile(string text) {
+            streamWriter.WriteLine(text);
+        }
+    }
+}
