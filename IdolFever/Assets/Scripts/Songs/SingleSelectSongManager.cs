de2f0d33c09281d735c376af444c2b9ee8f0eecb
Dragonstormthunder/Ezra <54053973+Dragonstormthunder@users.noreply.github.com>
2021-02-21 16:34:43 +0800

    single character screen

---
+++ IdolFever/Assets/Scripts/Songs/SingleSelectSongManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//using IdolFever.Server;
+
+namespace IdolFever
+{
+    public class SingleSelectSongManager : MonoBehaviour
+    {
+
+        #region Fields
+
+        public GameObject contentPanel;
+        //public ServerDatabase serverDatabase;
+        public GameObject songPrefab;
+
+        #endregion
+
+        private void Start()
+        {
+            string name = "";
+            int rating = 1;
+
+            // use the song registry to create all these items
+            for (SongRegistry.SongList index = 0; index < SongRegistry.SongList.NOT_OPTION; ++index)
+            {
+
+                // the song prefab
+                GameObject song = Instantiate(songPrefab, contentPanel.transform);
+
+                // initialize the song data
+                SongData songData = song.GetComponent<SongData>();
+                SongDataFactory.GenerateSongData(index, ref name, ref rating);
+                //Debug.Log("Song: " + name + " " + rating);
+                songData.SetSongData(name, rating);
+
+                // initialize the leaderboard controller
+                LeaderboardController leaderboardController = song.GetComponent<LeaderboardController>();
+                leaderboardController.SongIndex = index;
+
+                // initialize the zoom and enhance
+                UI.SongSelectionZoomAndEnhance songSelectionZoomAndEnhance = song.GetComponent<UI.SongSelectionZoomAndEnhance>();
+                songSelectionZoomAndEnhance.SongIndex = index;
+
+            }
+
+
+        }
+
+    }
+}
---
+++ IdolFever/Assets/Scripts/Songs/SingleSongSelectionEvents.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IdolFever
+{
+
+    public class SingleSongSelectionEvents : MonoBehaviour
+    {
+
+        #region Fields
+
+        public static SingleSongSelectionEvents INSTANCE;
+
+        #endregion
+
+        #region Unity Messages
+
+        // singleton
+        private void Awake()
+        {
+            INSTANCE = this;
+        }
+
+        #endregion
+
+        #region Events
+
+        #region Leaderboard Change Event
+
+        internal event Action<SongRegistry.SongList> onLeaderboardChange;
+        internal void LeaderboardChange(SongRegistry.SongList index)
+        {
+            // check if null
+            onLeaderboardChange?.Invoke(index);
+        }
+
+        #endregion
+
+        #region Zoom And Enhance Event
+
+        internal event Action<SongRegistry.SongList> onZoomAndEnhance;
+        internal void ZoomAndEnhance(SongRegistry.SongList index)
+        {
+            // check if null
+            onZoomAndEnhance?.Invoke(index);
+        }
+
+        #endregion
+
+
+        #endregion
+
+    }
+}
