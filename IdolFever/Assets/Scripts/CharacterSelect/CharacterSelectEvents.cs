2a989e0dab6dfe6645611cc4a56917c22ad42ad7
Dragonstormthunder/Ezra <54053973+Dragonstormthunder@users.noreply.github.com>
2021-02-22 12:00:02 +0800

    prelunch commit; character select scene

---
+++ IdolFever/Assets/Scripts/CharacterSelect/CharacterSelectEvents.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IdolFever
+{
+    public class CharacterSelectEvents : MonoBehaviour
+    {
+        #region Fields
+
+        public static CharacterSelectEvents INSTANCE;
+
+        #endregion
+
+        #region Unity Messages
+
+        // singleton
+        private void Awake()
+        {
+            INSTANCE = this;
+        }
+
+        #endregion
+
+        #region Events
+
+        #region Character Selected
+
+        //internal event Action<Character> onCharacterSwitched;
+        //internal void CharacterSwitched(Character.CharacterFactory.eCHARACTER index)
+        //{
+        //    onCharacterSwitched?.Invoke(index);
+        //}
+
+        #endregion
+
+        #endregion
+
+
+    }
+
+}
