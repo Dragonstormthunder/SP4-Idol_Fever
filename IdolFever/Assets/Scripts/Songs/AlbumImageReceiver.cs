3cbfe856b2dfc34a3f657d6f5b90ca328cc393b8
Dragonstormthunder <emmadecember2002@gmail.com>
2021-02-24 19:33:21 +0800

    test

---
+++ IdolFever/Assets/Scripts/Songs/AlbumImageReceiver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IdolFever
+{
+
+    public class AlbumImageReceiver : MonoBehaviour
+    {
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity Messages
+        #endregion
+
+        
+
+    }
+
+}
