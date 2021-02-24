2ca73221b5e9c9f8f895540f9495a6cd671565ae
Henry <193541T@mymail.nyp.edu.sg>
2021-02-24 23:15:03 +0800

    Test

--- IdolFever/Assets/Scripts/GuanYu/Trail.cs
+++ IdolFever/Assets/Scripts/GuanYu/Trail.cs
@@ -6,6 +6,9 @@ namespace IdolFever {
 
         [SerializeField] private float distFromCam;
         [SerializeField] private Camera camComponent;
+        [SerializeField] private GameObject parent;
+        [SerializeField] private GameObject particleSystemPrefab;
+        [SerializeField] private Material[] mtls;
 
         #endregion
 
@@ -13,13 +16,29 @@ namespace IdolFever {
         #endregion
 
         public Trail() {
+            distFromCam = 0.0f;
             camComponent = null;
+            parent = null;
+            particleSystemPrefab = null;
+            mtls = System.Array.Empty<Material>();
         }
 
         #region Unity User Callback Event Funcs
 
+        private void Start() {
+            ParticleSystem particleSystemComponentParent = parent.GetComponent<ParticleSystem>();
+
+            foreach(Material mtl in mtls) {
+                GameObject particleSystemGO = Instantiate(particleSystemPrefab);
+                particleSystemGO.transform.SetParent(parent.transform);
+
+                ParticleSystem particleSystemComponent = particleSystemGO.GetComponent<ParticleSystem>();
+                particleSystemComponent.GetComponent<Renderer>().material = mtl;
+                particleSystemComponentParent.subEmitters.AddSubEmitter(particleSystemComponent, ParticleSystemSubEmitterType.Manual, ParticleSystemSubEmitterProperties.InheritNothing);
+            }
+        }
+
         private void Update() {
-            distFromCam = 0.0f;
             MoveTrailToCursor(Input.mousePosition);
         }
 
