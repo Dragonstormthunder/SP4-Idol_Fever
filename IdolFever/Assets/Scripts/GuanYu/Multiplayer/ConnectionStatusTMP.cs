e1b66ad2d986938ae14058dff8ced05ce73338e3
Henry <193541T@mymail.nyp.edu.sg>
2021-02-25 16:14:52 +0800

    stuff

---
+++ IdolFever/Assets/Scripts/GuanYu/Multiplayer/ConnectionStatusTMP.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class ConnectionStatusTMP: MonoBehaviour {
+        #region Fields
+
+        private readonly string textFront = "Connection Status: ";
+        [SerializeField] private TextMeshProUGUI tmpComponent;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Update() {
+            tmpComponent.text = textFront + PhotonNetwork.NetworkClientState;
+        }
+
+        #endregion
+
+        public ConnectionStatusTMP() {
+            tmpComponent = null;
+        }
+    }
+}
