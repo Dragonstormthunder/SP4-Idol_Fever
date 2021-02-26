7ae111011e12c880c4446187f28c7b9c0f3311df
Dragonstormthunder <emmadecember2002@gmail.com>
2021-02-26 16:01:37 +0800

    firebase log out application quit

---
+++ IdolFever/Assets/Scripts/FirebaseServer/FirebaseLogOut.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+
+namespace IdolFever.Server
+{
+    public class FirebaseLogOut : MonoBehaviour
+    {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity Messages
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void OnApplicationQuit()
+        {
+            // sign out the user
+            FirebaseAuth.DefaultInstance.SignOut();
+        }
+
+        #endregion
+
+    }
+}
+
