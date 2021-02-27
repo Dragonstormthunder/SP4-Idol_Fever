6d18ad249a6f3248eec2f9350d738e56a79b1f9e
Dragonstormthunder <emmadecember2002@gmail.com>
2021-02-27 22:35:03 +0800

    song select button

--- IdolFever/Assets/Scripts/EnergyCost.cs
+++ IdolFever/Assets/Scripts/EnergyCost.cs
@@ -1,29 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class EnergyCost : MonoBehaviour
 {
 
     [SerializeField] IdolFever.Server.ServerDatabase b;
     [SerializeField] IdolFever.AsyncSceneTransitionOutWithAlts o;
+    private bool enoughEnergy = false;
+    public Image buttonImage;
+    public Button button;
 
     // Use this for initialization
     void Start()
     {
-
+        // update the number of gems
+        StartCoroutine(CheckEnoughEnergy());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (enoughEnergy == false)
+        {
+            button.transition = Selectable.Transition.None;
+            buttonImage.color = Color.gray;
+        }
+        else
+        {
+            button.transition = Selectable.Transition.ColorTint;
+            buttonImage.color = Color.white;
+        }
+    }
 
+    public IEnumerator CheckEnoughEnergy()
+    {
+        while (enoughEnergy == false)
+        {
+            _ = StartCoroutine(b.GetEnergy((d) =>
+              {
+                  if (d - 3 > 0)
+                  {
+                      enoughEnergy = true;
+                  }
+              }));
+
+            if (enoughEnergy)
+                break;
+            else
+                yield return new WaitForSeconds(1f);
+        }
+        yield return null;
     }
 
     public void ChangeEnergy(int i)
     {
-        StartCoroutine(b.GetEnergy((d) => {
+        StartCoroutine(b.GetEnergy((d) =>
+        {
             Debug.Log("Energy: " + (d).ToString());
-            if (d+i > 0)
+            if (d + i > 0)
             {
                 StartCoroutine(b.UpdateEnergy(d + i));
                 o.ChangeSceneByFlag();
