614697de3becf1522a1517060b505de61a72337f
Henry <193541T@mymail.nyp.edu.sg>
2021-02-27 15:46:48 +0800

    save

---
+++ IdolFever/Assets/Scripts/GuanYu/SongSelectedText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class SongSelectedText: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private TextMeshProUGUI tmpComponent;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            
+        }
+
+        #endregion
+    }
+}
