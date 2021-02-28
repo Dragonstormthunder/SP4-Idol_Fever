63d9ef8e3594a425b8bb93c54cd2254f3e7758bf
Henry <193541T@mymail.nyp.edu.sg>
2021-02-28 17:02:39 +0800

    yes

---
+++ IdolFever/Assets/Scripts/GuanYu/TrailActivity.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class TrailActivity: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private GameObject myTrail;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        internal TrailActivity() {
+            myTrail = null;
+        }
+
+        #region Unity User Callback Event Funcs
+
+        private void Update() {
+            if(UnityEngine.Rendering.SplashScreen.isFinished) {
+                myTrail.SetActive(true);
+                enabled = false;
+            }
+        }
+
+        #endregion
+    }
+}
