eca1fec17af333c31ed896d18fe8d64f5d068b90
Henry <193541T@mymail.nyp.edu.sg>
2021-02-28 22:22:57 +0800

    BGM.cs and some audio files

---
+++ IdolFever/Assets/Scripts/GuanYu/Audio/BGM.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace IdolFever {
+    internal sealed class BGM: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private AudioSource[] audioSrcs;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        internal BGM() {
+            audioSrcs = null;
+        }
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            audioSrcs[Random.Range(0, audioSrcs.Length)].enabled = true;
+        }
+
+        #endregion
+    }
+}
